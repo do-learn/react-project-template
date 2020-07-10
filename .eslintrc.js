13d5987232e4d35f029e97706f30191077a8ccda
huangyl <huangyl@tuya.com>
2020-07-10 11:25:15 +0800

    feat: eslint配置完成

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,59 +5,68 @@ module.exports = {
     jest: true,
   },
   globals: {},
-  parser: '@typescript-eslint/parser',
+  parser: "@typescript-eslint/parser",
   parserOptions: {
-    project: './tsconfig.json',
+    project: "./tsconfig.json",
+    sourceType: "module",
+    ecmaFeatures: {
+      jsx: true,
+    },
   },
   plugins: [
-    'react',
-    '@typescript-eslint',
-    'eslint-comments', // 对eslint-disable等注释进行规范
-    'jest',
-    'promise',
-    'unicorn',
+    "prettier",
+    "react",
+    "@typescript-eslint",
+    "eslint-comments", // 对eslint-disable等注释进行规范
+    "jest",
+    "promise",
+    "unicorn",
   ],
   extends: [
-    'airbnb-typescript',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:eslint-comments/recommended',
-    'plugin:jest/recommended',
-    'plugin:promise/recommended',
-    'plugin:unicorn/recommended',
-    'prettier',
-    'prettier/react',
-    'prettier/@typescript-eslint',
+    "airbnb-typescript",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:eslint-comments/recommended",
+    "plugin:jest/recommended",
+    "plugin:promise/recommended",
+    "plugin:unicorn/recommended",
+    "prettier",
+    "prettier/react",
+    "prettier/@typescript-eslint",
   ],
   rules: {
-    "indent": ["error", 2],
+    "prettier/prettier": "error",
+    indent: ["error", 2],
     // Too restrictive, writing ugly code to defend against a very unlikely scenario: https://eslint.org/docs/rules/no-prototype-builtins
-    'no-prototype-builtins': 'off',
+    "no-prototype-builtins": "off",
     // https://basarat.gitbooks.io/typescript/docs/tips/defaultIsBad.html
-    'import/prefer-default-export': 'off',
-    'import/no-default-export': 'error',
+    "import/prefer-default-export": "off",
+    "import/no-default-export": "error",
     // Too restrictive: https://github.com/yannickcr/eslint-plugin-react/blob/master/docs/rules/destructuring-assignment.md
-    'react/destructuring-assignment': 'off',
+    "react/destructuring-assignment": "off",
     // No jsx extension: https://github.com/facebook/create-react-app/issues/87#issuecomment-234627904
-    'react/jsx-filename-extension': 'off',
+    "react/jsx-filename-extension": "off",
     // Use function hoisting to improve code readability
-    'no-use-before-define': [
-      'error',
+    "no-use-before-define": [
+      "error",
       { functions: false, classes: true, variables: true },
     ],
     // Makes no sense to allow type inferrence for expression parameters, but require typing the response
-    '@typescript-eslint/explicit-function-return-type': [
-      'error',
+    "@typescript-eslint/explicit-function-return-type": [
+      "error",
       { allowExpressions: true, allowTypedFunctionExpressions: true },
     ],
-    '@typescript-eslint/no-use-before-define': [
-      'error',
+    "@typescript-eslint/no-use-before-define": [
+      "error",
       {
-        functions: false, classes: true, variables: true, typedefs: true,
+        functions: false,
+        classes: true,
+        variables: true,
+        typedefs: true,
       },
     ],
     // Common abbreviations are known and readable
-    'unicorn/prevent-abbreviations': 'off',
-    'import/no-default-export': 0,
-    'unicorn/filename-case': 0,
+    "unicorn/prevent-abbreviations": "off",
+    "import/no-default-export": 0,
+    "unicorn/filename-case": 0,
   },
 };
--- src/ToDo.tsx
+++ src/ToDo.tsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 const ToDo: React.FC = () => {
-  return <div>我欲乘风归去，又恐琼楼玉宇, 高处不胜寒，起舞弄清影</div>;
+  return (
+    <div style={{ color: "blue" }}>
+      我欲乘风归去，又恐琼楼玉宇, 高处不胜寒，起舞弄清影
+    </div>
+  );
 };
 
 export default ToDo;
